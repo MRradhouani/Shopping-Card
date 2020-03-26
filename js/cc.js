013e40508131a7c6863082ef94d42fffa300fe0c
MRradhouani <37581416+MRradhouani@users.noreply.github.com>
2020-03-26 23:38:01 +0100

    first commit

---
+++ js/cc.js
@@ -0,0 +1,14 @@
+let a = array.from(document.getElementById('article'));
+let total = document.getElementById('totla');
+
+function total() {
+    let sum = 0
+    for (let i = 0; i < total.clientHeight; i++)
+        total
+}
+
+function soum() {
+    let a = 0
+    for
+    a += parseInt()
+}
